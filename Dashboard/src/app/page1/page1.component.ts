39ae2e91e264a0c756289f5ed881bf00ee0d83bf
Antonstockmarr <s164170@student.dtu.dk>
2022-06-07 11:13:50 +0200

    Update images
    stop using faIcons and use local icons instead

--- Dashboard/src/app/page1/page1.component.ts
+++ Dashboard/src/app/page1/page1.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { faUsers,faTriangleExclamation, faServer, faFileContract} from '@fortawesome/free-solid-svg-icons';
 import { LocalStorageService } from '../services/local-storage.service';
 import { DataRepositoryService } from '../services/data-repository.service';
 import { Objective } from '../models/objective';
@@ -39,14 +38,8 @@ export class Page1Component implements OnInit {
 
   }
 
-  getIcon(objective: Objective) {
-    if (objective.name == "User Experience")
-      return faUsers;
-    if (objective.name == "Resource Management")
-      return faServer;
-    if (objective.name == "Incident Management")
-      return faTriangleExclamation
-    else return faUsers;
+  getIcon(objective: Objective): string {
+    return `assets/${objective.name}.png`;
   }
     
   presetConcerns(objective: Objective) {
